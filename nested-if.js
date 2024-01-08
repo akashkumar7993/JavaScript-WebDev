67c7fc82c20b1016558d890bad83eb64afe25524
Akash Kumar <123872296+akashkumar7993@users.noreply.github.com>
2024-01-08 23:53:38 +0530

    Update nested-if.js

--- nested-if.js
+++ nested-if.js
@@ -3,4 +3,40 @@ const readlineSynce = require("readline-sync")
 
 const number =Number(readlineSynce.question("Enter any Numbner: "))
 
-console.log(number);
+const remainder = number % 2;
+
+// if  (remainder === 0){
+//     console.log(`${number}: is an even number`);
+// } else {
+//     console.log(`${number}:is a Prime Number`)
+// }
+
+
+
+// if  (remainder === 0){
+//     console.log(`${number}: is an even number`);
+//     if (number % 4 === 0){
+//         console.log(`${number}: is dividible by 4`)
+//     }else {
+//         console.log(`${number}: is not dividible by 4`)
+//     }
+// } else {
+//     console.log(`${number}:is a Prime Number`)
+// }
+
+
+if  (remainder === 0){
+    console.log(`${number}: is an even number`);
+    if (number % 4 === 0){
+        console.log(`${number}: is dividible by 4`)
+    }else {
+        console.log(`${number}: is not dividible by 4`)
+    }
+} else {
+    console.log(`${number}:is a Prime Number`)
+    if (number % 5 === 0){
+        console.log(`${number}: is dividible by 5`)
+    }else {
+        console.log(`${number}: is not dividible by 5`)
+    }
+}

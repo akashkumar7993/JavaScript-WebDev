1663e662dd3b12f4b387de4605bda519263da34b
Akash Kumar <123872296+akashkumar7993@users.noreply.github.com>
2024-01-09 19:54:36 +0530

    Create Exercise-for-loop.js

---
+++ Exercise-for-loop.js
@@ -0,0 +1,31 @@
+/**
+ * Star print
+ 
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+
+*/
+
+// console.log("*");
+// console.log("**");
+// console.log("***");
+// console.log("****");
+// console.log("*****");
+
+
+const symbol = ("* ")
+// console.log(symbol.repeat(1));
+// console.log(symbol.repeat(2));
+// console.log(symbol.repeat(3));
+// console.log(symbol.repeat(4));
+// console.log(symbol.repeat(5));
+
+
+for(let i =1; i<=5 ; i++){
+    console.log(symbol.repeat(i));
+}

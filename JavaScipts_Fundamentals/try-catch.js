eac1cce4b4aca99ce5d9531eafbbd5c9bfafe4c0
Akash Kumar <123872296+akashkumar7993@users.noreply.github.com>
2024-01-10 00:00:14 +0530

    Create try-catch.js

---
+++ JavaScipts_Fundamentals/try-catch.js
@@ -0,0 +1,26 @@
+/*
+try catch block
+
+try{
+    some codes of the code....
+}catch (error) {
+    console.log(error)
+}
+*/ 
+
+
+// const username = "Akash";
+// console.log(myname);
+// console.log(username); // it will occur an error bcox our first line has'nt executed hence it did'nt goes to the second line.
+
+
+
+// trying with catch block 
+
+const username = "Akash";
+try{
+    console.log(myname);
+} catch(error){
+    console.log("error Occured");
+}
+console.log(username);

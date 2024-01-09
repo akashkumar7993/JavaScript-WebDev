718a5ad3f2b77745707dc770b149a9e91a0f1d0b
Akash Kumar <123872296+akashkumar7993@users.noreply.github.com>
2024-01-09 20:44:13 +0530

    Update Exercise-for-loop.js

--- Exercise-for-loop.js
+++ Exercise-for-loop.js
@@ -18,7 +18,7 @@
 // console.log("*****");
 
 
-const symbol = ("* ")
+// const symbol = ("* ")
 // console.log(symbol.repeat(1));
 // console.log(symbol.repeat(2));
 // console.log(symbol.repeat(3));
@@ -26,6 +26,57 @@ const symbol = ("* ")
 // console.log(symbol.repeat(5));
 
 
-for(let i =1; i<=5 ; i++){
-    console.log(symbol.repeat(i));
+
+
+// for(let i =1; i<=5 ; i++){
+//     console.log(symbol.repeat(i));
+// }
+
+
+
+//  revrse
+
+// for(let i=5; i>=1 ; i--){
+//     console.log(symbol.repeat(i));
+// }
+
+ 
+// Print chracter String count 
+
+// let userName = "Akash kumar Mahato";
+
+// let count = 0;
+// for(let i = 0; i< userName.length; i++){
+//     count++;
+// }
+
+// console.log(`Number of Characters in the String is ${count}`);
+
+
+/**
+ * Given a rangre of Number from 1 to 101, find all the even number
+ 
+ * **/
+
+// let remainder;
+// for (let i=1; i<=101; i++){
+//     remainder = i% 2;
+//     if (remainder === 0){
+//         console.log(`${i} is an even number`);
+//     }else {
+//         console.log(`${i} is a odd number`);
+//     }
+// }
+
+// Find Vowels 
+
+const inputString = "Hello i love to Eat Burger king burger";
+
+const vowel ="aeiou";
+for( let i = 0; i < inputString.length; i++){
+    if( vowel.includes(inputString[i])){
+        console.log(`${inputString[i]} is a vowel`);
+    }else{
+        console.log(`${inputString[i]} is not vowel`);
+    }
 }

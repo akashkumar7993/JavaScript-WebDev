4ed63a6184ecad0c3d16934db75fe3d1024c35ba
Akash Kumar <123872296+akashkumar7993@users.noreply.github.com>
2024-01-17 22:13:30 +0530

    update object

--- 6. Objects/optional-chaining.js
+++ 6. Objects/optional-chaining.js
@@ -1,3 +1,32 @@
 /*
-    Optional chaining
+    Optional chaining ?.
 */
+
+const user ={
+    name : "Akash",
+    age: 99,
+    // address: {//optional
+    //     street: "Dhanbad",
+    //     state: "Jharkhand",
+    //     city: "Dhanabad"
+    // },
+    likes: "Eat Code Sleep Repeat",
+    getDisplay: function(){
+        console.log("Welcome Akash")
+    }
+}
+
+// sol 1  if address in not provided and you searched
+
+// if(user.address !== undefined){
+//     console.log(user.address.city);
+// }else{
+//     console.log("Address not found");
+// }
+
+// sol2 
+
+console.log(user.address?.city)  // this ? will check address if onlu=y address having an object it will move forward and return undeifined
+
+user.getDisplay();
+user.getDisplayAddress?.();

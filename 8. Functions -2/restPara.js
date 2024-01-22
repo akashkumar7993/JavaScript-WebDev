40c4be693d2b598ff6a6af660e87f535152b1d0a
Akash Kumar <123872296+akashkumar7993@users.noreply.github.com>
2024-01-22 23:18:35 +0530

    update function

---
+++ 8. Functions -2/restPara.js
@@ -0,0 +1,11 @@
+/*
+    Rest Parameter---->
+*/
+
+function calculateTotal(a,b,...rest){
+    console.log(a);
+    console.log(b);
+    console.log(rest);
+    }
+    
+    calculateTotal(2,3,4,5,7,8,9,11.16);

2ec43d117ceb17b0c72fd798e9e644783fec184f
Akash Kumar <123872296+akashkumar7993@users.noreply.github.com>
2024-01-27 19:26:49 +0530

    added

--- 11. Projects/Form-Validation/app.js
+++ 11. Projects/Form-Validation/app.js
@@ -110,7 +110,7 @@ submitButton.addEventListener("click", (event) => {
 
   if (fnFlag && lnFlag && eFlag && pwdFlag){
     fnTarget.value = lnTarget.value = emailTarget.value = pwdTarget.value = "";
-    window.location.href = "/success.html"
+    window.location.href = "/success.html";
   }
 
 });

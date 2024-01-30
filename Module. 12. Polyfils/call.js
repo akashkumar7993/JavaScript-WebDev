1d7bb9858ba49a7919e81602381517c401dcac7c
Akash Kumar <123872296+akashkumar7993@users.noreply.github.com>
2024-01-30 21:28:21 +0530

    Update call.js

--- Module. 12. Polyfils/call.js
+++ Module. 12. Polyfils/call.js
@@ -13,4 +13,18 @@ const obj = {
 function displayuserinfo(state){
     console.log(`Hi ia am ${this.name} from ${this.city}, ${state}`);
 }
-displayuserinfo.call(obj, "Delhi");
+
+Function.prototype.mycall = function (context, ...args){
+    context.showMessage = this;
+    context.showMessage(...args);
+    delete context.showMessage;
+};
+
+Function.prototype.myApply = function(context, args){
+    context.showMessage = this;
+    context.showMessage(...args);
+    delete context.showMessage;
+}
+
+// displayuserinfo.call(obj, "Delhi");
+displayuserinfo.myApply(obj, ["Delhi"]);
